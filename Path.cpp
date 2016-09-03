dd7245c723e52b3e37352f48cc62c253b803dad3
Lee Fogg <leefogg@gmail.com>
2016-09-03 21:31:34 +0100

    Added documentation

--- Path.cpp
+++ Path.cpp
@@ -3,13 +3,21 @@
 
 #include "Path.h"
 
+/// <summary>
+/// Appends path2 to path1, ensuring a valid path format
+/// </summary>
 String Path::combinePaths(String path1, String path2) {
+	// TODO: replace \ with /
+	// TODO: normalise both paths
 	if (path1.endsWith("/"))
 		return path1 + path2;
 	else
 		return path1 + "/" + path2;
 }
 
+/// <summary>
+/// Appends a / to a folder path to ensure a format
+/// </summary>
 String Path::normalisePath(String path) {
 	if (Path::hasFile(path))
 		return path;
@@ -20,15 +28,29 @@ String Path::normalisePath(String path) {
 		return path + "/";
 }
 
+/// <summary>
+/// Checks whether path mentions a file
+/// </summary>
+/// <returns>True if a file exists in the given path</returns>
 bool Path::hasFile(String path) {
+	// TODO: check if content after the .
 	return path.indexOf('.') != -1;
 }
 
+/// <summary>
+/// Extracts file name from a path
+/// </summary>
+/// <return>File and extension from given path or path if no file exists</returns>
 String Path::getFileName(String path) {
 	Path::normalisePath(path);
+	// TODO: Check if file exists
 	return path.substring(path.lastIndexOf("\\") + 1);
 }
 
+/// <summary>
+/// Extracts a file's extension
+/// </summary>
+/// <returns>The file's extension or an empty string if no file is found in string</return>
 String Path::getFileExtension(String path) {
 	if (!Path::hasFile(path))
 		return "";
--- WebServer.ino
+++ WebServer.ino
@@ -1,6 +1,3 @@
-#define EthernetEnablePin 10
-#define SDEnablePin 4
-
 #include <SPI.h>
 #include <Ethernet.h>
 #include <SD.h>
@@ -10,32 +7,42 @@
 #include "ContentType.h"
 #include "Path.h"
 
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
-byte mac[] = {
-0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
-};
+// Chip Select pins for SD card and Ethernet driver on Arduino's Ethernet Shield.
+#define EthernetEnablePin 10
+#define SDEnablePin 4
+//TODO: define EthernetBufferSize 1024*2
 
 // Initialize the Ethernet server
-// (port 80 is default for HTTP):
+// port 80 is default for HTTP
 EthernetServer server(80);
 
+/// <summary>
+/// Both SD and Ethernet use SPI. Enable the Ethernet driver and disable the SD card.
+/// </summary>
 void enableEthernet() {
 	digitalWrite(SDEnablePin, HIGH);
 	digitalWrite(EthernetEnablePin, LOW);
 }
-
+/// <summary>
+/// Both SD and Ethernet use SPI. Enable the SD card and disable the Ethernet driver.
+/// </summary>
 void enableSD() {
 	digitalWrite(SDEnablePin, LOW);
 	digitalWrite(EthernetEnablePin, HIGH);
 }
 
+/// <summary>
+/// Struct to store data requested by client
+/// </summary>
 struct HTTPRequest {
 	byte Method;
 	String File;
 	boolean KeepAlive;
 	//TODO: Accepted types
 } Request;
+/// <summary>
+/// Struct to store data to be sent to client
+/// </summary>
 struct HTTPResponse {
 	unsigned int StatusCode;
 	boolean KeepAlive;
@@ -55,23 +62,37 @@ void setup() {
 	}
 
 	enableEthernet();
-	// start the Ethernet connection and the server:
-	Ethernet.begin(mac);
+	// Enter a unique MAC address below. This only has to be unique on the network.
+	byte macaddress[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+	// Start up the ethernet driver with MAC address and find the first available IP on network.
+	Ethernet.begin(macaddress);
 	server.begin();
 	server.available();
+	// Display the IP the web server is accessable on via the serial port.
 	Serial.println(Ethernet.localIP());
 }
 
+/// <summary>
+/// Gets a line of data from the client.
+/// Assumes the ethernet controller is active.
+/// </summary>
+/// <returns>The next available line of data</returns>
 String readLine(EthernetClient client) {
 	return client.readStringUntil('\n');
 }
 
+/// <summary>
+/// Clears data from the last HTTP request.
+/// </summary>
 void clearRequest() {
 	Request.File = "";
 	Request.Method = HTTPMethod::Get; // Assume get
 	Request.KeepAlive = false; // Close connection after by default
 }
 
+/// <summary>
+/// Clears data from the last HTTP response.
+/// </summary>
 void clearResponse() {
 	Response.ContentLength = 0;
 	Response.ContentType = "";
@@ -79,6 +100,9 @@ void clearResponse() {
 	Response.StatusCode = HTTPStatusCode::ClientError::BadRequest;
 }
 
+/// <summary>
+/// Interprets the incoming HTTP request header and fills the request struct.
+/// </summary>
 void readHTTPRequest(EthernetClient client) {
 	clearRequest();
 
@@ -110,8 +134,21 @@ void readHTTPRequest(EthernetClient client) {
 		if (line.indexOf("Connection") != -1)
 			Request.KeepAlive = line.indexOf("keep-alive") != -1;
 	}
+
+	// Show read request information
+	Serial.println();
+	Serial.print("Connection: ");
+	Serial.println(String(Request.KeepAlive));
+	Serial.print("Method: ");
+	Serial.println(String(Request.Method));
+	Serial.print("File: ");
+	Serial.println(Request.File);
+	Serial.println();
 }
 
+/// <summary>
+/// Uses the HTTP response struct to write a response header to the client.
+/// </summary>
 void writeResponseHeader(EthernetClient client) {
 	enableEthernet();
 
@@ -125,6 +162,8 @@ void writeResponseHeader(EthernetClient client) {
 	Serial.println(String(Response.ContentLength));
 	Serial.println();
 	
+	// Confirm what was written to the client by echoing in serial port
+	// TODO: Dont duplicate above, write exact string that was written to client to serial.
 	client.print("HTTP/1.0 ");
 	client.println(String(Response.StatusCode));
 	client.print("Content-Type: ");
@@ -136,25 +175,45 @@ void writeResponseHeader(EthernetClient client) {
 	client.println();
 }
 
+/// <summary>
+/// Writes a string of unknown length to the client.
+/// Use array version for performance reasons.
+/// </summary>
 void writeToEthernet(String text, EthernetClient client) {
 	enableEthernet();
+
+	// No nice way to work with unknow array lengths in C. Can only trust the String library here
+	// Much slower sending because sending byte at a time instead of passing an array
 	for (unsigned int i = 0; i < text.length(); i++)
 		client.write(text[i]);
 }
+/// <summary>
+/// Writes an array of bytes to the client.
+/// Currently benchmarked at 41KB per second.
+/// </summary>
 void writeToEthernet(byte* data, unsigned int size, EthernetClient client) {
 	enableEthernet();
 
 	unsigned int dataremaining = size;
 	while (dataremaining > 0) {
+		// Limit sent data to ethernet controllers buffer size
 		unsigned int datatosend = (dataremaining < 2048) ? dataremaining : 2048;
 
 		client.write(data, datatosend);
+		//TODO: flush
+
+		// Count how much data is remaining to be sent
 		data += datatosend;
 		dataremaining -= datatosend;
 	}
 }
 
+/// <summary>
+/// Send a file from the SD card to the client.
+/// </summary>
+/// <param name="filepath">Absolute file path of file on SD card.</param>
 void dumpFile(String filepath, EthernetClient client) {
+	//TODO: Return if file was sent
 	enableSD();
 
 	File file = SD.open(filepath);
@@ -167,41 +226,58 @@ void dumpFile(String filepath, EthernetClient client) {
 	dumpFile(file, client);
 	file.close();
 }
+/// <summary>
+/// Sends a file from the SD card to the client.
+/// </summary>
+/// <param name="file">An opened file from the SD card.</param>
 void dumpFile(File file, EthernetClient client) {
 	enableSD();
 
+	// Finish writing header and send to client
+	// TODO: Move to caller
 	Response.ContentLength = file.size();
 	writeResponseHeader(client);
 
 	Serial.println("Reading file..");
-	// Buffer the data
+	// Ethernet controller has a 2048 byte buffer
 	uint16_t const buffersize = 1024*2;
 	char buffer[buffersize];
 	while (file.available()) {
 		Serial.print("Reading ");
 		Serial.print(buffersize, DEC);
 		Serial.print(" bytes...");
-
+		// Read next 2K into RAM while ethernet controller is sending
 		int bytesread = file.read(buffer, buffersize);
 		Serial.print(" Read ");
 		Serial.print(bytesread, DEC);
 		Serial.println(" bytes.");
 
-		// Send buffered data
+		// TODO: Flush
+
+		// Send buffered data to controller
 		enableEthernet();
 		client.write(buffer, bytesread);
 
+		// Switch back to SD for further reading
 		enableSD();
 	}
 }
 
+/// <summary>
+/// Requests a file to be sent to the client.
+/// Sends a 404 error if the file does not exist.
+/// Sends 415 error if the file has unsupported extension.
+/// Constructs and sends appropriate HTTP header then file contents.
+/// </summary>
+/// <param name="filepath">The absolute file path of a file on the SD card.</param>
+/// <returns>True if the content of the requested file was sent to the client, otherwise, false.</returns>
 bool sendFile(String filepath, EthernetClient client) {
 	enableSD();
 
+	// Show Unsupported Media Type page if unknown file extension
 	String extension = Path::getFileExtension(filepath);
 	extension.toLowerCase();
 	String contenttype = ContentType::getTypeFromExtension(extension);
-
 	if (contenttype.length() == 0) {
 		Response.StatusCode = HTTPStatusCode::ClientError::UnsupportedMediaType;
 		Response.ContentType = "text/html";
@@ -209,6 +285,7 @@ bool sendFile(String filepath, EthernetClient client) {
 		return false;
 	}
 
+	// Show 404 page if file not found.
 	File file = SD.open(filepath);
 	if (!file) {
 		Response.StatusCode = HTTPStatusCode::ClientError::NotFound;
@@ -217,35 +294,43 @@ bool sendFile(String filepath, EthernetClient client) {
 		return false;
 	}
 
-	// Passed checks
+	// Passed checks.
 	Response.StatusCode = HTTPStatusCode::Success::OK;
 	Response.ContentType = contenttype;
-	dumpFile(file, client);
+
+	dumpFile(file, client);	// Write HTTP header and file contents
 	file.close();
 
 	return true;
 }
 
+/// <summary>
+/// Constructs and sends HTML code to client with classic directory listing of folder contents
+/// </summary>
+/// <param name="path">The absolute path of a folder on the SD card.</param>
 void showDirectoryListing(String path, EthernetClient client) { // TODO: Fix file listing changing bug
 	enableSD();
 
 	File folder = SD.open(path);
-	if (!folder) {
+	if (!folder) { // Open return null if file could not be found
 		Response.StatusCode = HTTPStatusCode::ClientError::NotFound;
 		Response.ContentType = "text/html";
 		dumpFile("404.htm", client);
 		return;
 	}
 	if (!folder.isDirectory()) {
-		//TODO: Send appropriate response code
+		//TODO: Check is directory before calling
 	}
 
+	// Passed validation checks
 	Response.StatusCode = HTTPStatusCode::Success::OK;
 	Response.ContentType = "text/html";
+
+	// Start constructing HTML code 
 	String content = "<HTML><HEAD><TITLE>" + Request.File + "</TITLE></HEAD><BODY><h1>Index of "+path+"</H1><TABLE><TR><TH>Name</TD><TH>Type</TD><TH>Size</TD></TR>";
 
 	File file;
-	while (file = folder.openNextFile()) {
+	while (file = folder.openNextFile()) { // Open each file in folder
 		content += "<TR>";
 		content += "<TD><A href = \"";
 		String filename = file.name();
@@ -267,43 +352,39 @@ void showDirectoryListing(String path, EthernetClient client) { // TODO: Fix fil
 		content += file.size();
 		content += "</TD>";
 		content += "<TR>";
-		file.close();
+		file.close(); // File must be closed!
 	}
-	folder.close();
+	folder.close(); // Close folder for reading.
 	
 	content += "</TABLE></BODY></HTML>";
 	Response.ContentLength = content.length();
 	
+	// Output constructed header and page
 	writeResponseHeader(client);
 	writeToEthernet(content, client);
 }
 
+/// <summary>
+/// Main loop which maintains Ethernet services and listens for incoming requests.
+/// </summary>
 void loop() {
+	// IP address is on lease, notify IP is still in use.
 	Ethernet.maintain();
 
-	// listen for incoming clients
-	EthernetClient client = server.available();
-	if (!client)
-		return;
+	// Listen for incoming clients
+	EthernetClient client = server.available(); // Get any avaiable clients
+	if (!client) // If null, no clients waiting.
+		return; // Loop again
 
 	while (client.connected()) {
 		if (client.available()) {
 			readHTTPRequest(client);
-
-			Serial.println();
-			Serial.print("Connection: ");
-			Serial.println(String(Request.KeepAlive));
-			Serial.print("Method: ");
-			Serial.println(String(Request.Method));
-			Serial.print("File: ");
-			Serial.println(Request.File);
-			Serial.println();
-
 			clearResponse();
 
+			// Tell client connection is still alive if requested to be
 			Response.KeepAlive = Request.KeepAlive;
-
-			if (Request.Method == HTTPMethod::Get) {
+			// If file was requested
+			if (Request.Method == HTTPMethod::Get) { // Server only supports Get requests currently
 				if (!Path::hasFile(Request.File)) { // Requested a folder or file without extension
 					showDirectoryListing(Request.File, client);
 				} else {
@@ -315,26 +396,16 @@ void loop() {
 				writeResponseHeader(client);
 			}
 
-			Serial.println();
-			Serial.print("Content-length: ");
-			Serial.println(String(Response.ContentLength));
-			Serial.print("Content-type: ");
-			Serial.println(String(Response.ContentType));
-			Serial.print("Keep-Alive: ");
-			Serial.println(Response.KeepAlive);
-			Serial.print("StatusCode: ");
-			Serial.println(Response.StatusCode);
-			Serial.println();
-
 			enableEthernet();
 			break;
 		}
 	}
 
+	// Wait for all sent bytes to be confirmed sent.
 	Serial.println("Flushing data stream...");
 	client.flush();
 
-	// Close the connection
-	client.stop();
+	// Close the connection to the client
+	client.stop(); // TODO: Investigate
 	Serial.println("Client disconnected.");
 }
